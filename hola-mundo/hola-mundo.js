ec523c07bc3e90307abcea0aabdd1f6d44e89256
jFroylan-Cal <j.froylan_calderon@outlook.com>
2022-12-16 15:44:36 -0600

    First commit

---
+++ hola-mundo/hola-mundo.js
@@ -0,0 +1,68 @@
+console.log("Hola mundo");
+// Tipos de dato
+
+//numericos
+console.log(12);
+
+console.log(12.21);
+
+//Caracteres cadenas
+//Cadena string
+console.log("Hola mundo");
+//caracter char
+console.log("H");
+
+//Booleanos
+console.log(true);
+console.log(false);
+
+//Indefinidos
+console.log(undefined);
+
+//nulos
+console.log(null);
+
+//Objetos
+objeto = { id: 1, name: "Jose", age: 30, things: null };
+
+console.log(objeto);
+
+//Arreglos
+myArrayNumeric = [1, 2, 3, 4, 5, 6, 7, 8];
+myArrayStrings = ["Hola", "Adios", "Buen día", "Buena noche"];
+myArrayNumericAndStrings = [myArrayNumeric, myArrayStrings];
+console.log(myArrayNumericAndStrings);
+myArrayObjects = [objeto, objeto];
+console.log(myArrayObjects);
+
+//Variables
+//const, var, let
+
+var nombre = 'Froy';
+
+var edad = 23;
+
+const pi = 3.1416;
+
+let perro = 'Huesos';
+
+var ciudad; 
+
+console.log(ciudad);
+
+//Operadores 
+
+//Operador de asignacion = 
+let x = 1;
+
+//Operador de asignacion de adicion += y sustraccion
+
+let y = 1;
+let z = 22;
+
+y += z;
+z -= y;
+console.log(y);
+console.log(z)
+
+//
